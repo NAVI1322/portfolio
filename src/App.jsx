3fa307fc5eae9ef124c15099b758306792c25ca1
NAVI1322 <rksharma041976@gmail.com>
2025-02-16 13:05:16 -0500

    custom cusor added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import LoadingScreen from '@/components/LoadingScreen'
 import LoadingSpinner from '@/components/LoadingSpinner'
+import AnimatedCursor from "react-animated-cursor"
 
 // Lazy load non-critical components
 const Skills = lazy(() => import('@/components/Skills'))
@@ -35,7 +36,27 @@ export default function App() {
   }
 
   return (
+    
     <TooltipProvider>
+       <AnimatedCursor innerSize={10}
+      outerSize={10}
+      color='193, 11, 110'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]} />
       <div className="min-h-screen w-full bg-background text-foreground antialiased transition-colors duration-300">
         {/* Progress bar */}
         <motion.div
